ab03bccc174e863b5622f705df4fe852323e6f83
Alexandre <alexandrehemm@gmail.com>
2024-04-14 18:33:42 -0300

    at

--- caixa_eletronico.py
+++ caixa_eletronico.py
@@ -11,9 +11,9 @@ class Conta_Bancaria():
         try:
             if float(valor)>0:
                 self.valor += valor
-                self.extrato.append(valor)
-                print(f"${valor} adicionado a conta")
-                print(f"Saldo atual: ${self.valor}")
+                self.extrato.append("{:.2f}".format(valor))
+                print(f"R${valor} adicionado a conta")
+                print(f"Saldo atual: R${"{:.2f}".format(self.valor)}")
             else:
                 raise
         except:
@@ -25,14 +25,14 @@ class Conta_Bancaria():
             print("**EMPTY**")
         else:
             for val in self.extrato:
-                print(f"${val}")
+                print(f"R${"{:.2f}".format(float(val))}")
     
     def check_withdraw(self):
         ac = 0
         if not self.extrato:
             return 0
         for v in self.extrato:
-            if v < 0:
+            if float(v) < 0:
                 ac+=1
         return ac
         
@@ -42,6 +42,7 @@ class Conta_Bancaria():
             if valor<0:
                 raise
             if self.check_withdraw()<3:
+         
                 if valor > 500:
                     print(f"O valor informado esta a cima do limite de $500")
                 else:
@@ -49,9 +50,9 @@ class Conta_Bancaria():
                         print(f"O valor informado esta a cima do saldo atual de ${self.valor}")
                     else:
                         self.valor -= float(valor)
-                        print(f"${valor} retirado da conta")
-                        print(f"Saldo atual ${self.valor}")
-                        self.extrato.append(-1*(valor))
+                        print(f"R${valor} retirado da conta")
+                        print(f"Saldo atual R${"{:.2f}".format(self.valor)}")
+                        self.extrato.append("{:.2f}".format(-1*valor))
             else:
                 print("Numero de saques excedido")
         except:
