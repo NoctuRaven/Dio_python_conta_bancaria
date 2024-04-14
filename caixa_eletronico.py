927349bdccd1fa941f8f0eb5b42588045aa1728e
Alexandre <alexandrehemm@gmail.com>
2024-04-14 18:06:56 -0300

    at

--- caixa_eletronico.py
+++ caixa_eletronico.py
@@ -26,19 +26,34 @@ class Conta_Bancaria():
         else:
             for val in self.extrato:
                 print(f"${val}")
+    
+    def check_withdraw(self):
+        ac = 0
+        if not self.extrato:
+            return 0
+        for v in self.extrato:
+            if v < 0:
+                ac+=1
+        return ac
         
     
     def withdraw_value(self,valor):
         try:
             if valor<0:
-                raise 
-            if (self.valor < valor ):
-                print(f"O valor informado esta a cima do saldo atual de ${self.valor}")
+                raise
+            if self.check_withdraw()<3:
+                if valor > 500:
+                    print(f"O valor informado esta a cima do limite de $500")
+                else:
+                    if (self.valor < valor ):
+                        print(f"O valor informado esta a cima do saldo atual de ${self.valor}")
+                    else:
+                        self.valor -= float(valor)
+                        print(f"${valor} retirado da conta")
+                        print(f"Saldo atual ${self.valor}")
+                        self.extrato.append(-1*(valor))
             else:
-                self.valor -= float(valor)
-                print(f"${valor} retirado da conta")
-                print(f"Saldo atual ${self.valor}")
-                self.extrato.append(-1*(valor))
+                print("Numero de saques excedido")
         except:
             print("Impossivel concluir a transação: Digite um valor válido")
                 
@@ -49,7 +64,12 @@ nova_conta = Conta_Bancaria(nome="Pessoa",cpf="123456789",valor=0.00)
 nova_conta.add_value(30)
 nova_conta.add_value(-30)
 nova_conta.withdraw_value(10)
+nova_conta.add_value(700)
+nova_conta.withdraw_value(600)
 nova_conta.add_value("aaa")
 nova_conta.withdraw_value(10000)
 nova_conta.withdraw_value(-10000)
+nova_conta.withdraw_value(10)
+nova_conta.withdraw_value(10)
+nova_conta.withdraw_value(10)
 nova_conta.show_extract()
